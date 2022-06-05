603e5d5ff7a2d10c18261b792539ed6f45d6c488
jeonggu Kang <93317914+jgkang9402@users.noreply.github.com>
2022-06-05 20:54:11 +0900

    '.'

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -34,10 +34,5 @@ const ProductDetail = () => {
   );
 };
 
-{/* <img src={item?.img} />
-<div>{item?.choice == true?"conscious choice":""}</div>
-<div>{item?.title}</div>
-<div>\{item?.price}</div>
-<div>{item?.new == true?"신제품":""}</div> */}
 
 export default ProductDetail;
